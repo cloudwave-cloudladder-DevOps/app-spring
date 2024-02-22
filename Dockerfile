e7279e5e1c782864e87f6c7f17e837f0b2b697ec
hann0079 <haeun8701@gmail.com>
2024-02-22 18:12:16 +0900

    edit config

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM openjdk:17-jdk-slim-buster
 WORKDIR /app
 
 # gitignore에 build가 ignore 처리되어있기 때문에 jar파일을 Docker파일이 있는 디렉토리로 복사 시킨뒤 git push
-COPY order-0.0.1-SNAPSHOT.jar app.jar
+COPY build/libs/order-0.0.1-SNAPSHOT.jar app.jar
 
 CMD ["java", "-jar", "app.jar"]
--- src/main/java/com/example/order/controller/MainController.java
+++ src/main/java/com/example/order/controller/MainController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    @GetMapping("/")
+    public String redirectToMain() {
+        return "redirect:/main";
+    }
+
     @GetMapping("/main")
     public String main() {
         return "order-page";
