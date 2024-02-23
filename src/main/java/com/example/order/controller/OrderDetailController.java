fda9a0f5fde8c7a73a656c67eff8e4c87d9e9c30
hann0079 <haeun8701@gmail.com>
2024-02-23 17:48:16 +0900

    - cors

--- src/main/java/com/example/order/controller/OrderDetailController.java
+++ src/main/java/com/example/order/controller/OrderDetailController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
