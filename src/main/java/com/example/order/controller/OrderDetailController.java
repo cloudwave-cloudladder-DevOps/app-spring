e07303be20d8e041cbc938317ebbab7897337db9
hann0079 <haeun8701@gmail.com>
2024-02-23 14:41:22 +0900

    commit

--- src/main/java/com/example/order/controller/OrderDetailController.java
+++ src/main/java/com/example/order/controller/OrderDetailController.java
@@ -6,7 +6,13 @@ import com.example.order.service.OrderDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -15,9 +21,12 @@ public class OrderDetailController {
     private final OrderDetailService orderDetailService;
 
     @PostMapping("/order")
-    @ResponseBody
-    public ResponseEntity<String> order(@RequestBody OrderRequestDto orderRequestDto) {
+    public ResponseEntity<?> order(@RequestBody OrderRequestDto orderRequestDto) {
         orderDetailService.createOrder(orderRequestDto);
-        return ResponseEntity.ok("성공적으로 주문이 완료되었습니다.");
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "성공적으로 주문이 완료되었습니다.");
+
+        return ResponseEntity.ok(response);
     }
 }
